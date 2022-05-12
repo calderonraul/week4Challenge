0bb5918c23fb759d828df1051589554ae6ad8c3d
Calderonraul <raulcalderon@ravn.co>
2022-05-12 15:06:09 -0600

    Recycler view, with view types finished

--- app/src/main/java/com/example/week4challenge/photo/PhotoAdapter.kt
+++ app/src/main/java/com/example/week4challenge/photo/PhotoAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.week4challenge.photo
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,35 +12,65 @@ import com.example.week4challenge.databinding.PhotoAdapterBinding
 import com.example.data.model.Photo
 import com.example.domain.entity.PhotoDomain
 import com.example.week4challenge.databinding.PhotoAdapterViewtype2Binding
+import java.lang.Exception
 
 class PhotoAdapter(val clickListener: PhotoClickListener) :
-    RecyclerView.Adapter<PhotoAdapter.PhotoViewHolder>() {
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
+    private lateinit var bindingType1: PhotoAdapterBinding
+    private lateinit var bindingType2: PhotoAdapterViewtype2Binding
     var photoList: List<PhotoDomain> = ArrayList()
+
+    enum class PhotoType(val value: Int) {
+        FIRST_VALUE(1), SECOND_VALUE(2)
+    }
+
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
-    ): PhotoAdapter.PhotoViewHolder {
-        val viewBinding: PhotoAdapterBinding = DataBindingUtil.inflate(
-            LayoutInflater.from(parent.context),
-            R.layout.photo_adapter, parent, false
-        )
-        return PhotoViewHolder(viewBinding)
-    }
+    ): RecyclerView.ViewHolder =
+        when (viewType) {
+            PhotoType.FIRST_VALUE.value -> {
+                bindingType1 = DataBindingUtil.inflate(
+                    LayoutInflater.from(parent.context),
+                    R.layout.photo_adapter, parent, false
+                )
+                Log.wtf("rfcj", "cast del tipo 1$bindingType1")
 
-    override fun onBindViewHolder(holder: PhotoAdapter.PhotoViewHolder, position: Int) {
-        holder.onBind(position)
+                PhotoViewHolder(bindingType1)
+            }
+            PhotoType.SECOND_VALUE.value -> {
+                bindingType2 = DataBindingUtil.inflate(
+                    LayoutInflater.from(parent.context),
+                    R.layout.photo_adapter_viewtype2, parent, false
+
+                )
+                Log.wtf("rfcj", "cast del tipo 2$bindingType2")
+                PhotoViewHolder2(bindingType2)
+            }
+            else -> {
+                throw Exception("couldn't create view holder")
+            }
+        }
+
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        return if (photoList[position].id % 2 == 0) {
+            (holder as PhotoViewHolder).onBind(position)
+        } else {
+            (holder as PhotoViewHolder2).onBind(position)
+        }
     }
 
     override fun getItemCount(): Int {
         return photoList.size
     }
 
-    fun setPhotos(photos: List<PhotoDomain>?) {
-        if (photos != null) {
-            this.photoList = photos
-        }
-        notifyDataSetChanged()
+
+    override fun getItemViewType(position: Int): Int {
+        return if (photoList[position].id % 2 == 0) PhotoType.FIRST_VALUE.value
+        else PhotoType.SECOND_VALUE.value
+
     }
 
     inner class PhotoViewHolder(private val viewBinding: PhotoAdapterBinding) :
@@ -51,4 +82,22 @@ class PhotoAdapter(val clickListener: PhotoClickListener) :
             viewBinding.photoClickListenerInterface = clickListener
         }
     }
+
+    inner class PhotoViewHolder2(private val viewBinding2: PhotoAdapterViewtype2Binding) :
+        RecyclerView.ViewHolder(viewBinding2.root) {
+        fun onBind(position: Int) {
+            val row = photoList[position]
+            viewBinding2.photo = row
+            viewBinding2.imgPortada2
+            viewBinding2.photoClickListenerInterface = clickListener
+        }
+    }
+
+    fun setPhotos(photos: List<PhotoDomain>?) {
+        if (photos != null) {
+            this.photoList = photos
+        }
+        notifyDataSetChanged()
+    }
+
 }
